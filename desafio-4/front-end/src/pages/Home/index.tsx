5cd1654b2d9570a277a56dc54570efd576dd1700
VictrCruz312 <motoggb979@gmail.com>
2023-03-06 14:38:20 -0300

    feat: Iniciando estrutura base front-end do desafio-4 em react

---
+++ desafio-4/front-end/src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HomeStyled } from "./styles";
+
+const Home: React.FC = () => {
+  return <HomeStyled></HomeStyled>;
+};
+
+export default Home;
---
+++ desafio-4/front-end/src/services/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8000",
+  timeout: 5000,
+});
+
+export const getEstabeleciments = async () => {
+  return api.get("/mongodb/estabelecimentos").then(({ data }) => data);
+};
+
+export const createEstabeleciments = async (data: any) => {
+  return api.post("/mongodb/estabelecimentos", data).then(({ data }) => data);
+};
+
+export const updateEstabeleciments = async (data: any, id: string) => {
+  return api
+    .patch(`/mongodb/estabelecimentos/${id}`, data)
+    .then(({ data }) => data);
+};
+
+export const deleteEstabeleciments = async (id: string) => {
+  return api.delete(`/mongodb/estabelecimentos/${id}`).then(({ data }) => data);
+};
